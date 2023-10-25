2372cd9e2727bf963db5ea697365f02bcf9c856c
yeongo <yeongo@student.42seoul.kr>
2023-10-25 19:59:23 +0900

    memcpy memmove trim

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: yeongo <yeongo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 11:36:11 by yeongo            #+#    #+#             */
-/*   Updated: 2022/07/08 09:05:45 by yeongo           ###   ########.fr       */
+/*   Updated: 2022/07/28 00:44:22 by yeongo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned char		*tmp_dst;
-	const unsigned char	*tmp_src = (const unsigned char *)src;
+	unsigned char	*tmp_dst;
+	unsigned char	*tmp_src;
 
 	if (dst == NULL && src == NULL)
 		return (NULL);
 	tmp_dst = (unsigned char *)dst;
+	tmp_src = (unsigned char *)src;
 	while (n--)
 		*tmp_dst++ = *tmp_src++;
 	return (dst);
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: yeongo <yeongo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 17:02:12 by yeongo            #+#    #+#             */
-/*   Updated: 2022/07/07 15:54:31 by yeongo           ###   ########.fr       */
+/*   Updated: 2022/07/28 00:44:28 by yeongo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	unsigned char		*tmp_dst;
-	const unsigned char	*tmp_src = (const unsigned char *)src;
+	unsigned char	*tmp_dst;
+	unsigned char	*tmp_src;
 
 	if (dst < src)
 		ft_memcpy(dst, src, len);
 	else if (dst > src)
 	{
 		tmp_dst = (unsigned char *)dst;
+		tmp_src = (unsigned char *)src;
 		while (len--)
 			*(tmp_dst + len) = *(tmp_src + len);
 	}
